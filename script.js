7ef7a348cb7594e413da178151aa433e8593326c
seventygrand <155555829+seventygrand@users.noreply.github.com>
2024-02-10 15:05:03 -0600

    lets hope this fixes it

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-<script>alert("By clicking OK You agree to being tracked for analytics purposes, if you do not agree, please exit the website by clicking X on the top of your Browser")</script>
+<script>alert("By clicking OK You agree to being tracked for analytics purposes, if you do not agree, please exit the website by clicking X on the top of your Browser");</script>
