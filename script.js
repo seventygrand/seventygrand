175e5a2cf949a18b253a34e5ef06aa80086d9ca7
seventygrand <155555829+seventygrand@users.noreply.github.com>
2024-02-10 15:00:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function changeColor() {
-  var heading = document.getElementById('heading');
-  heading.style.color = 'red';
-}
+ prompt(By clicking OK You agree to being tracked for analytics purposes, if you do not agree, please exit the website by clicking X on the top of your Browser)
